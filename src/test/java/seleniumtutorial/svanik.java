c7fdd7511c129c0399389d635a0673e91c90ff8e
narsimha78 <narsimha78@gmail.com>
2015-09-22 19:57:44 +1000

    svanik class is added

---
+++ src/test/java/seleniumtutorial/svanik.java
@@ -0,0 +1,13 @@
+package seleniumtutorial;
+
+import org.testng.annotations.Test;
+
+public class svanik {
+	
+	@Test
+	public void reddy(){
+		
+		System.out.println("My name Svanik and i am sweet boy");
+	}
+
+}
