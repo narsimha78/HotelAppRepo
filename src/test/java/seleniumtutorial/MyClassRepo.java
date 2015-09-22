b734113c901c8273b7958b3dd0cf63283548684c
narsimha78 <narsimha78@users.noreply.github.com>
2015-09-22 20:03:08 +1000

    myClass Repo

---
+++ src/test/java/seleniumtutorial/MyClassRepo.java
@@ -0,0 +1,13 @@
+package seleniumtutorial;
+
+import org.testng.annotations.Test;
+
+public class MyClassRepo {
+	
+	@Test
+	public void ClassRepo(){
+		
+		System.out.println("A class created on Repository on WEb");
+	}
+
+}
